2056357519735054866760468148b13190a7a57b
Tat Lay <GratitudeNow@Tat.local>
2019-03-18 10:22:05 +0000

    task 2 completed

---
+++ 1-types-challenges/solutions/2.js
@@ -0,0 +1,13 @@
+// Without changing the variable values, join the strings together with a space in between (create a separate file for each version):
+
+// Using concatenation
+// Using interpolation
+
+let string1 = "I",
+    string2 = "like",
+    string3 = "to",
+    string4 = "boogie"
+    string5 = "boogie";
+
+let joined = string1+" "+string2+" "+string3+" "+string4+" "+string5; // your code here
+console.log(joined); // "I like to boogie boogie"

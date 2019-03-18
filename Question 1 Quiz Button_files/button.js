1f5b657054664ade8512bd7ace462c0813879ceb
Tat Lay <GratitudeNow@Tat.local>
2019-03-18 10:08:14 +0000

    All challenges

---
+++ Question 1 Quiz Button_files/button.js
@@ -0,0 +1,16 @@
+((d) => {
+    let count = 0;
+    let button = d.getElementById("button")
+
+    button.addEventListener("click", () =>
+
+    	count += 50
+
+
+    	);
+
+})(document);
+
+//let get elements by id
+//
+//style.top += 50

83cdcc299a03a4f3670c14eb751cfd19a519cf22
Tat Lay <GratitudeNow@Tat.local>
2019-03-19 16:33:24 +0000

    Write some code that lists the numbers from 1 to 100 using a for loop
    Write some code that lists the numbers from 100 to 1 using a for loop
    
    Write some code that lists all multiples of 13 up to 1,000 using a for loop
    
    Write some code that lists all multiples of 13 up to 1,000 using a while loop
    
    Write some code that outputs any even numbers between 1 and 50 that are also divisible by 3 COMPLETED

---
+++ conditionals-loops-challenges/attempt/4.js
@@ -0,0 +1,8 @@
+// Write some code that lists all multiples of 13 up to 1,000 using a while loop
+
+let total = 0;
+
+while (total <= 987) {
+	total += 13;
+	console.log(total);
+}
---
+++ conditionals-loops-challenges/attempt/5.js
@@ -0,0 +1,8 @@
+// Write some code that outputs any even numbers between 1 and 50 that are also divisible by 3
+
+for (let i = 1; i <= 50; i += 1) {
+		if (i % 2 === 0 && i % 3 === 0){
+		console.log(i);		
+
+	}
+}

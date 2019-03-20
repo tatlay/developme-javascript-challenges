aae177af2e3791d2666cbfa3b121579bdf2ff01b
Tat Lay <GratitudeNow@Tat.local>
2019-03-20 09:17:02 +0000

    true / false

--- functions-basic-challenges/attempt/13_odd_true_false.js
+++ functions-basic-challenges/attempt/13_odd_true_false.js
@@ -1,6 +1,7 @@
 // Create a function, odd, that takes a number as an argument. The function should return true if the number is odd and false if it is even.
 
 let odd = value =>{
+	return value % 2 !== 0 ? true : false;
 	
 }
 
